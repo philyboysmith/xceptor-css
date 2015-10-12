eb2792521ae2d9d277acae51ad48efe6720e1688
Brian Young <brian@dotdreaming.com>
2015-10-12 09:13:03 +0100

    headerbar and navigation header changes (menu)

--- js/mock-up-ui.js
+++ js/mock-up-ui.js
@@ -27,3 +27,56 @@ $('.ui-panel-header').on('click', function(e){
     });
     e.stopPropagation();
 });
+
+
+
+//---- navigation header navigation dd
+
+var mainNav = {
+	menuHeight:  "",
+	menuOpen: "false",
+	init: function () {
+		var _this = this;
+		_this.menuHeight =  _this.getMenuHeight();
+
+		// initia
+		$('.ui-navigation-header__nav-list').css('margin-top', - _this.menuHeight);
+
+		//this.closeMenu();
+		$('.ui-navigation-header__title').on('click', function(e){
+			e.preventDefault();
+			
+			var $clickedNavTitle = $(this);
+
+		    $clickedNavTitle.parent('.ui-navigation-header__nav').toggleClass('ui-navigation-header__nav--open');
+		    if (_this.menuOpen === false) {
+		    	// open menu
+		    	_this.openMenu($clickedNavTitle );
+		    
+		    } else {
+		    	// close menu
+		    	_this.closeMenu($clickedNavTitle , _this.menuHeight);
+		    }
+		   
+
+		});
+
+
+	},
+	getMenuHeight: function() {
+		return $('.ui-navigation-header__nav-list').height();
+	},
+	openMenu: function(navTitle) {
+		var _this = this;
+		navTitle.parent('.ui-navigation-header__nav').find('.ui-navigation-header__nav-list').css('margin-top', '0');
+		_this.menuOpen = true;
+	},
+	closeMenu: function(navTitle, menuHeight) {
+		var _this = this;
+		navTitle.parent('.ui-navigation-header__nav').find('.ui-navigation-header__nav-list').css('margin-top', - menuHeight);
+		_this.menuOpen = false;
+	}
+
+};
+
+mainNav.init();
